49bf365a2757ee59776bf7b999b4928832e29a07
gattlin1 <gattlin28@gmail.com>
2020-02-14 15:53:33 -0600

    Added heatmaps and also deleted files that got messed up when merging.

--- NASA_Version/spectra_comparison/heatmap.py
+++ NASA_Version/spectra_comparison/heatmap.py
@@ -25,29 +25,30 @@ def generate_heatmaps(file_path, floor_values, wavelength_values, file_count):
             accuracy, avg_miss, class_lvl = line.split(sep=',', maxsplit=2)
 
             class_lvl = class_lvl.replace('[', '').replace(']', '').split(sep=',')
-
             accuracy = float(accuracy)
             avg_miss = float(avg_miss)
-            class_lvl = [int(lvl) for lvl in class_lvl]
+            class_lvl = [(float(lvl) / file_count * 100) for lvl in class_lvl]
             entries['accuracy'].append(accuracy)
             entries['avg miss'].append(avg_miss)
             entries['class lvl'].append(class_lvl)
 
-    file_name = file_path.split('/')[3:][0].replace('heatmap.txt', '')
+    file_name = file_path.split('/')[4:][0].replace('heatmap.txt', '')
 
     accuracy_heatmap = np.array(entries['accuracy']).reshape((len(floor_values), len(wavelength_values))).round(decimals=2)
     title = file_name + 'Best Match Accuracy'
-    create_heatmap(title, accuracy_heatmap, floor_values, wavelength_values)
+    create_heatmap(title, np.rot90(accuracy_heatmap, k=1, axes=(0,1)), floor_values, wavelength_values)
 
     avg_miss_heatmap = np.array(entries['avg miss']).reshape((len(floor_values), len(wavelength_values))).round(decimals=2)
     title = file_name + 'Average Best Match Miss'
-    create_heatmap(title, avg_miss_heatmap, floor_values, wavelength_values)
+    create_heatmap(title, np.rot90(avg_miss_heatmap, k=1, axes=(0,1)), floor_values, wavelength_values)
 
     class_lvl_heatmap = np.array(entries['class lvl'])
-    for i in range(1, class_lvl_heatmap.shape[1]):
+    for i in range(1, np.size(class_lvl_heatmap,1)):
         title = file_name + 'Accuracy at Level {0}'.format(i)
-        lvl_heatmap = class_lvl_heatmap[:, i] / file_count * 100
-        create_heatmap(title, lvl_heatmap.round(decimals=2), floor_values, wavelength_values)
+        lvl_heatmap = class_lvl_heatmap[:, i].reshape((len(floor_values), len(wavelength_values)))
+        print(np.rot90(lvl_heatmap.round(decimals=2)))
+        #lvl_heatmap = np.true_divide(class_lvl_heatmap,  file_count)
+        create_heatmap(title, np.rot90(lvl_heatmap.round(decimals=2), k=1, axes=(0,1)), floor_values, wavelength_values)
 
 
 def create_heatmap(title, heatmap, floor_values, wavelength_values):
@@ -64,21 +65,22 @@ def create_heatmap(title, heatmap, floor_values, wavelength_values):
     ax.set_xticklabels(floor_values)
 
     ax.set_ylabel('Wavelength')
-    ax.set_yticklabels(wavelength_values)
+    ax.set_yticklabels(wavelength_values[::-1])    
 
     # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
             rotation_mode="anchor")
 
     # Loop over data dimensions and create text annotations.
-    for i in range(len(floor_values)):
-        for j in range(len(heatmap)):
+    for i in range(len(wavelength_values)):
+        for j in range(len(floor_values)):
             text = ax.text(j, i, heatmap[i, j],
                         ha="center", va="center", color="w")
 
     ax.set_title(title)
     fig.tight_layout()
-    plt.savefig(plt.savefig('../heatmaps/{0}.png'.format(title)))
+    plt.savefig('../heatmaps/{0}.png'.format(title))
+    plt.close()
 
 if __name__=='__main__':
     start = datetime.datetime.now()
@@ -95,30 +97,30 @@ if __name__=='__main__':
     wavelength_values = [0.2, 0.4, 0.6, 0.8, 1.0]
     i = 1
 
-    # For loop to iterate through each
-    for f_val in floor_values:
-        for w_val in wavelength_values:
+    # # For loop to iterate through each
+    # for f_val in floor_values:
+    #     for w_val in wavelength_values:
 
-            # Create NLC Files for each hitlist
-            make_nlc_files(dataset_path, nlc_dataset_path, floor_value=f_val, width=w_val)
+    #         # Create NLC Files for each hitlist
+    #         make_nlc_files(dataset_path, nlc_dataset_path, floor_value=f_val, width=w_val)
 
-            # Start Multiprocessing
-            processes = []
-            hitlist_types = ['cor', 'dpn', 'mad', 'msd', 'nlc - cor', 'nlc - dpn', 'nlc - mad', 'nlc - msd']
-            for alg in hitlist_types:
-                p = multiprocessing.Process(target=run_hitlist, args=(alg, nlc_dataset_path, i))
-                processes.append(p)
-                p.start()
+    #         # Start Multiprocessing
+    #         processes = []
+    #         hitlist_types = ['cor', 'dpn', 'mad', 'msd', 'nlc - cor', 'nlc - dpn', 'nlc - mad', 'nlc - msd']
+    #         for alg in hitlist_types:
+    #             p = multiprocessing.Process(target=run_hitlist, args=(alg, nlc_dataset_path, i))
+    #             processes.append(p)
+    #             p.start()
 
-            for process in processes:
-                process.join()
-                process.terminate()
+    #         for process in processes:
+    #             process.join()
+    #             process.terminate()
 
-            i += 1
+    #         i += 1
 
     path = '../results/6th_run/heatmap/'
-    file_count = list(os.listdir(nlc_dataset_path))
+    file_count = len(list(os.listdir(nlc_dataset_path)))
     for file in os.listdir(path):
-        generate_heatmaps(path, floor_values, wavelength_values)
+        generate_heatmaps(path + file, floor_values, wavelength_values, file_count)
 
     print('Total Runtime: {0}'.format(datetime.datetime.now() - start))
